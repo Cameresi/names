9a18e4c60e57c6b6f8faa4f7791dd3a65b8e02da
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-23 16:17:53 -0400

    version 2

--- names.rb
+++ names.rb
@@ -1,4 +1,37 @@
-first_name = "Aaron" 
-last_name = "Cameresi"
 
-print "#{first_name} #{last_name}"
+# def firstname(first_name = "Aaron")
+# 	print "#{first_name}"
+# end
+
+# def lastname(last_name = " Cameresi")
+# 	print "#{last_name}"
+# end
+
+# def names
+# 	firstname
+# 	lastname
+# end
+
+# names
+
+
+def get_firstname()
+
+	print "Enter your first name: "
+	firstname = gets.chomp
+end
+
+def get_lastname()
+
+	print "Enter your last name: "
+	lastname = gets.chomp
+end
+
+ def print_name
+ 	firstname = get_firstname()
+ 	lastname = get_lastname()
+ print "My name is #{firstname} #{lastname}"
+ end
+
+
+print_name
